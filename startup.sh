00e813c7c9e1a7ffff0eddbef8baf997c1d512e5
Akshat <162790900+MrTech-AK@users.noreply.github.com>
2025-01-31 16:59:20 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,7 +9,7 @@ Copyright (c) 2021 ThePro-CoderZ | @TheArjvps
 "
 
 start_nexaub () {
-    if [[ -z "$PYRO_STR_SESSION" ]]
+    if [[ -z "BQAi31UAYLP7FHM6uPj8yB-XfCBcJqYCfw3dZY1_PEzTX1HYZgo0_JeAWLLWYK6Jzwqpt593bfk44rDTBrsG6ndmE5Zhx030BTE-2nKO4KoFLOetp_7-z_BbFzQ1frRb4yqkCscRPVYyu_CytfLJ1Dlobvn7mGcEPSQYvMr0eabqz4wBY9HD8vOiPOQekO1qkUF2C2tiJHqWEZN_ikEPvE4ygSIFj8a1aBdKYwFgWajb5KTT1R9X1LK9ngwVqMD_uoCAGNa-oq0MYMkoHCvzoTAd1swmD1Ocjeonl8FOrpLHjEOKf_z1wE5-C00Pv--FDO8PJHE3osb9vdt3bvtbrwy3NwFTTQAAAAHeAf3AAA" ]]
     then
 	    echo "Please add Pyrogram String Session"
     else
