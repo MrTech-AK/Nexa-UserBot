3f5cb4da5fd29aff815537d5cb0e6e1056a920e0
Akshat <162790900+MrTech-AK@users.noreply.github.com>
2025-01-31 16:02:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 class Config(object):
-    APP_ID = 2285397
-    API_HASH = "636fc37d9384a91c61619953cd760b48"
-    PYRO_STR_SESSION = "BQAi31UAYLP7FHM6uPj8yB-XfCBcJqYCfw3dZY1_PEzTX1HYZgo0_JeAWLLWYK6Jzwqpt593bfk44rDTBrsG6ndmE5Zhx030BTE-2nKO4KoFLOetp_7-z_BbFzQ1frRb4yqkCscRPVYyu_CytfLJ1Dlobvn7mGcEPSQYvMr0eabqz4wBY9HD8vOiPOQekO1qkUF2C2tiJHqWEZN_ikEPvE4ygSIFj8a1aBdKYwFgWajb5KTT1R9X1LK9ngwVqMD_uoCAGNa-oq0MYMkoHCvzoTAd1swmD1Ocjeonl8FOrpLHjEOKf_z1wE5-C00Pv--FDO8PJHE3osb9vdt3bvtbrwy3NwFTTQAAAAHeAf3AAA"
-    CMD_PREFIX = "."
-    MONGODB_URL = "mongodb+srv://pagac28482:JaltQe2DsAuJkLkO@cluster0.jm0vf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    APP_ID = os.environ.get("APP_ID", "")
+    API_HASH = os.environ.get("API_HASH", "")
+    PYRO_STR_SESSION = os.environ.get("PYRO_STR_SESSION", "")
+    CMD_PREFIX = os.environ.get("CMD_PREFIX", ".")
+    MONGODB_URL = os.environ.get("MONGODB_URL", "")
